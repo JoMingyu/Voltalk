e8e1bdf5d7d98153d234eab447b860616a218ba4
JoMingyu <city7310@naver.com>
2017-04-07 16:19:39 +0900

    info 모듈 생성

---
+++ enertalk_infos/info.py
@@ -0,0 +1,2 @@
+URL = 'http://api2.enertalk.com/'
+headers = headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer 7c1cbc4c116f033c41d1e47b2caa0f957e9d1a3f89d2c7caefadb6a1017998ff2626b0ad986f72c6ac16be64386ffd3863a8a181754c16060d923ec92cb5f290'}
